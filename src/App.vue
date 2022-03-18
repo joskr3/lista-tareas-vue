dd8852752cd9aa929a2433e44c2f9b4179a17046
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-17 21:32:53 -0500

    trabajando con vue

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+export default {
+  el: '#app',
+  data() {
+    return {
+      estaEditando: false,
+      tarea: "",
+      tareas: [],
+      tareaSeleccionada: null
+    }
+  },
+  methods: {
+    guardarTarea() {
+      if (this.tarea.trim() === "") {
+        return;
+      }
+      this.tareas.push( this.tarea );
+      this.tarea = "";
+    },
+    eliminarTarea( index ) {
+      this.tareas.splice( index, 1 );
+    },
+    actualizarTarea() {
+      this.tareas.splice( this.selectedIndex, 1, this.tarea );
+      this.estaEditando = false;
+      this.tarea = "";
+    },
+    editarTarea( index, tarea ) {
+      this.estaEditando = true;
+      this.tarea = tarea;
+      this.selectedIndex = index;
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app" class="container">
+    <h1>Lista de tareas</h1>
+    <div v-if=" !estaEditando ">
+      <input type="text" v-model=" tarea " />
+      <button type="submit" @click=" guardarTarea ">Agregar</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div v-else>
+      <input type="text" v-model=" tarea " />
+      <button type="submit" @click=" actualizarTarea ">Actualizar</button>
+    </div>
+    <ol>
+      <li v-for="(  tarea, index  ) in   tareas  " :key=" index ">
+        {{ tarea }}
+        <button
+          class="button button-outline"
+          @click=" editarTarea( index, tarea ) "
+        >Editar</button>
+        <button class="button button-clear" @click=" eliminarTarea( index ) ">Eliminar</button>
+      </li>
+    </ol>
+  </div>
 </template>
-
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
