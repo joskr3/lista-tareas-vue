a8703c3cdfc3383976fef1130e761c202af54ae4
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-17 22:02:43 -0500

    refactrorizando a composition API

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,48 @@
 <script >
+import { ref } from 'vue'
+
 export default {
-  el: '#app',
-  data() {
-    return {
-      estaEditando: false,
-      tarea: "",
-      tareas: [],
-      tareaSeleccionada: null
-    }
-  },
-  methods: {
-    guardarTarea() {
-      if (this.tarea.trim() === "") {
+  setup() {
+    const estaEditando = ref( false );
+    const tarea = ref( "" );
+    const tareas = ref( [] );
+    const tareaSeleccionada = ref( null );
+
+    function guardarTarea() {
+      if ( tarea.value.trim() === "" ) {
         return;
       }
-      this.tareas.push( this.tarea );
-      this.tarea = "";
-    },
-    eliminarTarea( index ) {
-      this.tareas.splice( index, 1 );
-    },
-    actualizarTarea() {
-      this.tareas.splice( this.selectedIndex, 1, this.tarea );
-      this.estaEditando = false;
-      this.tarea = "";
-    },
-    editarTarea( index, tarea ) {
-      this.estaEditando = true;
-      this.tarea = tarea;
-      this.selectedIndex = index;
+      tareas.value.push( tarea );
+     // tarea.value = "";
+    }
+
+    function eliminarTarea( index ) {
+      tareas.value.splice( index, 1 );
+    }
+
+    function actualizarTarea( index ) {
+      tareas.value.splice( index, 1, tarea );
+      estaEditando.value = false;
+      tarea.value = "";
+    }
+
+    function editarTarea( index, tarea ) {
+      estaEditando.value = true;
+      tarea.value = tarea
+      tareaSeleccionada.value = index;
+      // selectedIndex = index;
+    }
+
+    return {
+      // properties that the template needs should be returned here
+      estaEditando,
+      tarea,
+      tareas,
+      tareaSeleccionada,
+      guardarTarea,
+      eliminarTarea,
+      actualizarTarea,
+      editarTarea
     }
   }
 }
@@ -43,17 +57,22 @@ export default {
     </div>
     <div v-else>
       <input type="text" v-model=" tarea " />
-      <button type="submit" @click=" actualizarTarea ">Actualizar</button>
+      <button type="submit" selected index="index" @click=" actualizarTarea( index ) ">Actualizar</button>
     </div>
     <ol>
-      <li v-for="(  tarea, index  ) in   tareas  " :key=" index ">
+      <li v-for = "( tarea, index) in  tareas" :key=" index ">
         {{ tarea }}
         <button
           class="button button-outline"
           @click=" editarTarea( index, tarea ) "
+          selected
+          index="{{ index }}"
         >Editar</button>
         <button class="button button-clear" @click=" eliminarTarea( index ) ">Eliminar</button>
       </li>
     </ol>
   </div>
 </template>
+
+<style  scoped>
+</style>
